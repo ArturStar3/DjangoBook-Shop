c552f0cf66b45b263a2b192b6b177bc1a50f24db
ArturStar3 <artur.chviire@mail.ru>
2024-03-13 22:24:53 +0500

    Shop_StripePayment

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
+
 
 def order_create(request):
     cart = Cart(request)
@@ -17,7 +19,10 @@ def order_create(request):
             cart.clear()
             # Запустить асинхронное задание
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            # Задать закза в сенсе
+            request.session['order_id'] = order.id
+            # Перенаправить к платежу
+            return redirect(reverse('payment:process'))
         
     else:
         form = OrderCreateForm()
